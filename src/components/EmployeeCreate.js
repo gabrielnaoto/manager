be15e361c349df91c3b67ebfab71528414a3aeb4
Gabriel <gabriel.naoto@jungsoft.com.br>
2019-05-28 16:15:01 -0300

    Create employee form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './commons'
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
   render() {
+    const { name, phone, shift, employeeUpdate } = this.props;
+
     return (
       <Card>
         <CardSection>
           <Input
             label="Name"
-            placeholder="John Doe" />
+            placeholder="John Doe"
+            value={name}
+            onChangeText={value => employeeUpdate({ prop: 'name', value })} />
         </CardSection>
 
         <CardSection>
           <Input
             label="Phone"
-            placeholder="55 5555-5555" />
+            placeholder="55 5555-5555"
+            value={phone}
+            onChangeText={value => employeeUpdate({ prop: 'phone', value })} />
         </CardSection>
 
         <CardSection>
 
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={() => console.log('oii')}>
+            Save
+          </Button>
         </CardSection>
       </Card>
     );
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employee;
+
+  return {
+    name,
+    phone,
+    shift
+  };
+}
+
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import AuthReducer from './AuthReducer'
+import AuthReducer from './AuthReducer';
+import EmployeeReducer from './EmployeeReducer';
 
 export default combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  employee: EmployeeReducer,
 });
